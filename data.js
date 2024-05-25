c0e3359920063c888ec1502c5779535483a2754e
geoseo360 <geoseo360@gmail.com>
2024-05-25 19:04:04 +0400

    commit

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall",
+      "name": "hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.46372623870951557,
+        "pitch": -0.009875861845101142,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2200046913639886,
+          "pitch": 0.097266459054687,
+          "rotation": 0,
+          "target": "3-bathroom"
+        },
+        {
+          "yaw": 0.10059921032949859,
+          "pitch": 0.0596850938315896,
+          "rotation": 0,
+          "target": "2-balcony"
+        },
+        {
+          "yaw": 0.42045410078020673,
+          "pitch": 0.3833117382765714,
+          "rotation": 0,
+          "target": "1-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-studio",
+      "name": "studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5844960210412502,
+        "pitch": -0.017999293211374834,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2366761553630461,
+          "pitch": 0.11576169818336624,
+          "rotation": 0,
+          "target": "2-balcony"
+        },
+        {
+          "yaw": -2.5865230269147492,
+          "pitch": 0.19281927886265748,
+          "rotation": 0,
+          "target": "0-hall"
+        },
+        {
+          "yaw": -2.84093580033287,
+          "pitch": 0.17250689187954116,
+          "rotation": 4.71238898038469,
+          "target": "3-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-balcony",
+      "name": "balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.7582494027734192,
+        "pitch": 0.06204171474566422,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1155546202072433,
+          "pitch": 0.13725210386856368,
+          "rotation": 0,
+          "target": "1-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5406609655917265,
+        "pitch": 0.0018238333137894358,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.020988971500120357,
+          "pitch": 0.28359662111698114,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
